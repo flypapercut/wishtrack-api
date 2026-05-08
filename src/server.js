a67811d99d883879bfd6e9df93d71c56c923904d
flypapercut <flypapercut@proton.me>
2026-05-08 14:58:54 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 const server = express();
+
+server.use(cors());
+
+server.use(express.json());
 
 server.use(routes);
 
